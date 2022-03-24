126013df4c13687d7039ee8729f5db75589edcb6
LuoYi <langisme@qq.com>
2022-03-24 22:16:46 +0800

    addexample: multdrawindirect

---
+++ examples/ch7_4_multidrawindirect.rs
@@ -0,0 +1,306 @@
+/*
+ * Copyright � 2012-2015 Graham Sellers
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+use sb7::prelude::*;
+
+const NUM_DRAWS: usize = 50000;
+
+#[rustfmt::skip]
+struct DrawArraysIndirectCommand {
+    count:          u32,   // count of vertices in a instance
+    prim_count:     u32,   // count of instances to render
+    first:          u32,   // first vertices in a instance to render
+    base_instance:  u32,   // first instance to render
+}
+
+#[derive(Default)]
+#[rustfmt::skip]
+struct App {
+    render_program:         u32,
+    object:                 Object,
+    indirect_draw_buffer:   u32,
+    draw_index_buffer:      u32,
+
+    uniforms:               Uniforms,
+
+    mode:                   Mode,
+    paused:                 bool,
+    vsync:                  bool,
+}
+
+#[derive(Default)]
+#[repr(C)]
+struct Uniforms {
+    time: i32,
+    view_matrix: i32,
+    proj_matrix: i32,
+    viewproj_matrix: i32,
+}
+
+#[derive(Debug)]
+enum Mode {
+    MultiDraw,
+    SeparateDraws,
+}
+
+impl Default for Mode {
+    fn default() -> Self {
+        Mode::MultiDraw
+    }
+}
+
+impl Mode {
+    fn toggle(&mut self) {
+        *self = match self {
+            Mode::MultiDraw => Mode::SeparateDraws,
+            Mode::SeparateDraws => Mode::MultiDraw,
+        }
+    }
+}
+
+impl App {
+    fn load_shaders(&mut self) {
+        self.render_program = program::link_from_shaders(
+            &[
+                shader::load(
+                    "media/shaders/multidrawindirect/render.vs.glsl",
+                    gl::VERTEX_SHADER,
+                    true,
+                ),
+                shader::load(
+                    "media/shaders/multidrawindirect/render.fs.glsl",
+                    gl::FRAGMENT_SHADER,
+                    true,
+                ),
+            ],
+            true,
+        );
+
+        unsafe {
+            let get_location = |name| {
+                let name = std::ffi::CString::new(name).unwrap();
+                gl::GetUniformLocation(self.render_program, name.as_ptr())
+            };
+
+            self.uniforms.time = get_location("time");
+            self.uniforms.view_matrix = get_location("view_matrix");
+            self.uniforms.proj_matrix = get_location("proj_matrix");
+            self.uniforms.viewproj_matrix = get_location("viewproj_matrix");
+        }
+    }
+}
+
+impl Application for App {
+    fn init(&self) -> AppConfig {
+        AppConfig {
+            title: "OpenGL SuperBible - Asteroids".into(),
+            ..Default::default()
+        }
+    }
+
+    fn startup(&mut self) {
+        self.load_shaders();
+
+        self.object.load("media/objects/asteroids.sbm");
+
+        unsafe {
+            gl::BindVertexArray(self.object.get_vao());
+
+            gl::GenBuffers(1, &mut self.indirect_draw_buffer);
+            gl::BindBuffer(gl::DRAW_INDIRECT_BUFFER, self.indirect_draw_buffer);
+            gl::BufferData(
+                gl::DRAW_INDIRECT_BUFFER,
+                (NUM_DRAWS * size_of::<DrawArraysIndirectCommand>()) as _,
+                null(),
+                gl::STATIC_DRAW,
+            );
+
+            let cmd: *mut DrawArraysIndirectCommand = gl::MapBufferRange(
+                gl::DRAW_INDIRECT_BUFFER,
+                0,
+                (NUM_DRAWS * size_of::<DrawArraysIndirectCommand>()) as _,
+                gl::MAP_WRITE_BIT | gl::MAP_INVALIDATE_BUFFER_BIT,
+            ) as _;
+
+            for i in 0..NUM_DRAWS {
+                let cmd = cmd.add(i);
+                ((*cmd).first, (*cmd).count) = self
+                    .object
+                    .get_sub_object_info(i % self.object.get_sub_object_count() as usize);
+                (*cmd).prim_count = 1;
+                (*cmd).base_instance = i as _;
+            }
+
+            gl::UnmapBuffer(gl::DRAW_INDIRECT_BUFFER);
+
+            gl::BindVertexArray(self.object.get_vao());
+
+            gl::GenBuffers(1, &mut self.draw_index_buffer);
+            gl::BindBuffer(gl::ARRAY_BUFFER, self.draw_index_buffer);
+            gl::BufferData(
+                gl::ARRAY_BUFFER,
+                (NUM_DRAWS * size_of::<u32>()) as _,
+                null(),
+                gl::STATIC_DRAW,
+            );
+
+            let draw_index: *mut u32 = gl::MapBufferRange(
+                gl::ARRAY_BUFFER,
+                0,
+                (NUM_DRAWS * size_of::<u32>()) as _,
+                gl::MAP_WRITE_BIT | gl::MAP_INVALIDATE_BUFFER_BIT,
+            ) as _;
+
+            for i in 0..NUM_DRAWS {
+                *draw_index.add(i) = i as u32;
+            }
+
+            gl::UnmapBuffer(gl::ARRAY_BUFFER);
+
+            gl::VertexAttribIPointer(10, 1, gl::UNSIGNED_INT, 0, 0 as _);
+            gl::VertexAttribDivisor(10, 1);
+            gl::EnableVertexAttribArray(10);
+
+            gl::Enable(gl::DEPTH_TEST);
+            gl::DepthFunc(gl::LEQUAL);
+
+            gl::Enable(gl::CULL_FACE);
+        }
+    }
+
+    fn render(&self, current_time: f64) {
+        let one = 1.0f32;
+        let black = [0.0, 0.0, 0.0, 1.0f32].as_ptr();
+
+        static mut LAST_TIME: f64 = 0.0;
+        static mut TOTAL_TIME: f64 = 0.0;
+
+        unsafe {
+            if !self.paused {
+                TOTAL_TIME += current_time - LAST_TIME;
+            }
+            LAST_TIME = current_time;
+
+            let t = TOTAL_TIME as f32;
+            // let i = (TOTAL_TIME * 3.) as i32;
+
+            gl::ClearBufferfv(gl::COLOR, 0, black);
+            gl::ClearBufferfv(gl::DEPTH, 0, &one);
+
+            let view_matrix = lookat(
+                vec3!(
+                    100.0 * (t * 0.023).cos(),
+                    100.0 * (t * 0.023).cos(),
+                    300.0 * (t * 0.037).sin() - 600.0
+                ),
+                vec3!(0., 0., 260.),
+                vec3!(0.1 - (t * 0.1).cos() * 0.3, 1.0, 0.0).normalize(),
+            );
+            let AppConfig { width, height, .. } = self.info();
+            let proj_matrix = perspective(50., width as f32 / height as f32, 0.1, 2000.0);
+            let viewproj_matrix = proj_matrix * view_matrix;
+
+            gl::UseProgram(self.render_program);
+
+            gl::Uniform1f(self.uniforms.time, t);
+            gl::UniformMatrix4fv(
+                self.uniforms.view_matrix,
+                1,
+                gl::FALSE,
+                addr_of!(view_matrix) as _,
+            );
+            gl::UniformMatrix4fv(
+                self.uniforms.proj_matrix,
+                1,
+                gl::FALSE,
+                addr_of!(proj_matrix) as _,
+            );
+            gl::UniformMatrix4fv(
+                self.uniforms.viewproj_matrix,
+                1,
+                gl::FALSE,
+                addr_of!(viewproj_matrix) as _,
+            );
+
+            gl::BindVertexArray(self.object.get_vao());
+
+            match self.mode {
+                Mode::MultiDraw => {
+                    gl::MultiDrawArraysIndirect(gl::TRIANGLES, null(), NUM_DRAWS as _, 0);
+                }
+                Mode::SeparateDraws => {
+                    for i in 0..NUM_DRAWS {
+                        let (first, count) = self
+                            .object
+                            .get_sub_object_info(i % self.object.get_sub_object_count() as usize);
+                        gl::DrawArraysInstancedBaseInstance(
+                            gl::TRIANGLES,
+                            first as _,
+                            count as _,
+                            1,
+                            i as _,
+                        );
+                    }
+                }
+            }
+        }
+    }
+
+    #[rustfmt::skip]
+    fn ui(&mut self, ui: &imgui::Ui) {
+        if let Some(win) = imgui::Window::new("Settings")
+            .position([10., 10.], imgui::Condition::Appearing)
+            .begin(ui)
+        {
+            #[inline(always)]
+            fn toggle_btn(id: i32, ui: &imgui::Ui, action: &mut dyn FnMut() -> ()) {
+                let id = ui.push_id(id);
+                if ui.button("Toggle") {
+                    action();
+                };
+                id.end();
+            }
+
+            toggle_btn(0, &ui, &mut|| {
+                self.mode.toggle();
+            });
+            ui.same_line(); ui.text(format!("Current Mode: {:?}", self.mode));
+
+            toggle_btn(1, &ui,&mut || {
+                self.paused = !self.paused;
+            });
+            ui.same_line(); ui.text(format!("Paused: {:?}", self.paused));
+
+            toggle_btn(2, &ui,&mut || {
+                self.vsync = !self.vsync;
+                self.set_vsync(self.vsync);
+            });
+            ui.same_line(); ui.text(format!("Vsync: {:?}", self.vsync));
+
+            win.end();
+        };
+    }
+}
+
+fn main() {
+    App::default().run();
+}
--- src/application.rs
+++ src/application.rs
@@ -1,5 +1,3 @@
-
-use std::{sync::Mutex};
 use once_cell::sync::OnceCell;
 
 use imgui_glfw_rs::glfw;
@@ -9,115 +7,143 @@ use glfw::{Action, Context, Key};
 
 #[derive(Debug, Clone)]
 pub struct AppConfig {
-  pub title:  String,
-  pub width:  usize,
-  pub height: usize,
+    pub title: String,
+    pub width: usize,
+    pub height: usize,
+    pub flags: AppFlags,
+    pub glfw: glfw::Glfw,
+}
+
+#[derive(Default, Debug, Clone)]
+pub struct AppFlags {
+    pub fullscreen: bool,
+    pub vsync: bool,
+    pub cursor: bool,
+    pub stereo: bool,
+    pub debug: bool,
+    pub robust: bool,
 }
 
 impl Default for AppConfig {
-  fn default() -> Self {
-    Self { title:  String::from("OpenGL SuperBible Example"),
-           width:  800,
-           height: 600, }
-  }
+    fn default() -> Self {
+        Self {
+            title: String::from("OpenGL SuperBible Example"),
+            width: 800,
+            height: 600,
+            flags: Default::default(),
+            glfw: glfw::init(glfw::FAIL_ON_ERRORS).unwrap(),
+        }
+    }
 }
 
-static INFO: OnceCell<Mutex<AppConfig>> = OnceCell::new();
+static mut INFO: OnceCell<AppConfig> = OnceCell::new();
 
 pub trait Application {
-  fn init(&self) -> AppConfig {
-    AppConfig::default()
-  }
-
-  fn info(&self) -> AppConfig {
-    INFO.get().unwrap().lock().unwrap().clone()
-  }
-
-  fn ui(&mut self, _ui: &imgui::Ui) { }
-
-  fn run(&mut self) {
-    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
-    glfw.window_hint(glfw::WindowHint::ContextVersion(4, 6));
-    glfw.window_hint(glfw::WindowHint::OpenGlProfile(glfw::OpenGlProfileHint::Core));
-
-    let info = {
-      INFO.set(Mutex::new(self.init())).unwrap();
-      INFO.get().unwrap().lock().unwrap().clone()
-    };
-
-    let (mut window, events) =
-      glfw.create_window(info.width as u32, info.height as u32, &info.title, glfw::WindowMode::Windowed)
-          .expect("Failed to create GLFW window.");
-    gl::load_with(|s| window.get_proc_address(s));
-
-    let mut imgui = imgui::Context::create();
-    imgui.set_ini_filename(None);
-    let mut imgui_glfw = imgui_glfw_rs::ImguiGLFW::new(&mut imgui, &mut window);
-
-    super::gl! {
-      gl::Viewport(0, 0, info.width as i32, info.height as i32);
+    fn init(&self) -> AppConfig {
+        AppConfig::default()
     }
 
-    std::mem::drop(info);
-
-    window.set_all_polling(true);
-    window.make_current();
-
-    self.startup();
-
-    while !window.should_close() {
-      glfw.poll_events();
-      for (_, event) in glfw::flush_messages(&events) {
-        imgui_glfw.handle_event(&mut imgui, &event);
-        self.handle_window_event(&mut window, event);
-      }
-
-      self.render(glfw.get_time());
-
-      let ui = imgui_glfw.frame(&mut window, &mut imgui);
-
-      self.ui(&ui);
-
-      imgui_glfw.draw(ui, &mut window);
-  
-      window.swap_buffers();
+    fn info(&self) -> AppConfig {
+        unsafe { INFO.get().unwrap().clone() }
     }
 
-    self.shutdown();
-  }
+    fn ui(&mut self, _ui: &imgui::Ui) {}
 
-  fn startup(&mut self) {}
-  fn render(&self, current_time: f64) {
-    super::gl! {
-      let g = (current_time.sin() * 0.5 + 0.5) as f32;
-      gl::ClearBufferfv(gl::COLOR, 0, [g, g, g, 1.0f32].as_ptr());
-    }
-  }
-  fn shutdown(&mut self) {}
+    fn run(&mut self) {
+        let info = unsafe {
+            INFO.set(self.init()).unwrap();
+            INFO.get_mut().unwrap()
+        };
 
-  fn on_resize(&mut self, _w: i32, _h: i32) {}
+        let glfw = &mut info.glfw;
+        glfw.window_hint(glfw::WindowHint::ContextVersion(4, 6));
+        glfw.window_hint(glfw::WindowHint::OpenGlProfile(
+            glfw::OpenGlProfileHint::Core,
+        ));
 
-  fn on_key(&mut self, _key: Key, _press: Action) {}
+        let (mut window, events) = glfw
+            .create_window(
+                info.width as u32,
+                info.height as u32,
+                &info.title,
+                glfw::WindowMode::Windowed,
+            )
+            .expect("Failed to create GLFW window.");
 
-  fn handle_window_event(&mut self, window: &mut glfw::Window, event: glfw::WindowEvent) {
-    match event {
-      glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
-        window.set_should_close(true)
-      }
-      glfw::WindowEvent::Key(key, _, action, _) => {
-        self.on_key(key, action);
-      }
-      glfw::WindowEvent::Size(w, h) => unsafe {
-        gl::Viewport(0, 0, w, h);
-        {
-          let mut lck = INFO.get().unwrap().lock();
-          let info = lck.as_mut().unwrap();
-          info.width = w as _;
-          info.height = h as _;
+        gl::load_with(|s| window.get_proc_address(s));
+
+        let mut imgui = imgui::Context::create();
+        imgui.set_ini_filename(None);
+        let mut imgui_glfw = imgui_glfw_rs::ImguiGLFW::new(&mut imgui, &mut window);
+
+        super::gl! {
+          gl::Viewport(0, 0, info.width as i32, info.height as i32);
         }
-        self.on_resize(w, h);
-      },
-      _ => {}
+
+        window.set_all_polling(true);
+        window.make_current();
+
+        self.startup();
+
+        while !window.should_close() {
+            glfw.poll_events();
+            for (_, event) in glfw::flush_messages(&events) {
+                imgui_glfw.handle_event(&mut imgui, &event);
+                self.handle_window_event(&mut window, event);
+            }
+
+            self.render(glfw.get_time());
+
+            let ui = imgui_glfw.frame(&mut window, &mut imgui);
+
+            self.ui(&ui);
+
+            imgui_glfw.draw(ui, &mut window);
+
+            window.swap_buffers();
+        }
+
+        self.shutdown();
     }
-  }
+
+    fn startup(&mut self) {}
+    fn render(&self, current_time: f64) {
+        super::gl! {
+          let g = (current_time.sin() * 0.5 + 0.5) as f32;
+          gl::ClearBufferfv(gl::COLOR, 0, [g, g, g, 1.0f32].as_ptr());
+        }
+    }
+    fn shutdown(&mut self) {}
+
+    fn on_resize(&mut self, _w: i32, _h: i32) {}
+
+    fn on_key(&mut self, _key: Key, _press: Action) {}
+
+    fn handle_window_event(&mut self, window: &mut glfw::Window, event: glfw::WindowEvent) {
+        match event {
+            glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+                window.set_should_close(true)
+            }
+            glfw::WindowEvent::Key(key, _, action, _) => {
+                self.on_key(key, action);
+            }
+            glfw::WindowEvent::Size(w, h) => unsafe {
+                gl::Viewport(0, 0, w, h);
+                {
+                    let info = INFO.get_mut().unwrap();
+                    info.width = w as _;
+                    info.height = h as _;
+                }
+                self.on_resize(w, h);
+            },
+            _ => {}
+        }
+    }
+
+    fn set_vsync(&mut self, enable: bool) {
+        let info = unsafe { INFO.get_mut().unwrap() };
+        info.flags.vsync = enable;
+        info.glfw
+            .set_swap_interval(glfw::SwapInterval::Sync(if enable { 1 } else { 0 }));
+    }
 }
